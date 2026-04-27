bdf3b5778fee3d88197863d520a82e6e32d0eed0
YUE <125772117+yusufEU@users.noreply.github.com>
2026-04-27 22:32:49 +0300

    Improve landing layout №1
    27.04.2026

--- script.js
+++
@@ -1,6 +0,0 @@
-const activeBtn = document.querySelector("#active-form");
-const form = document.querySelector("#formJS");
-
-activeBtn.addEventListener("click", () => {
-	form.classList.toggle("form-active")
-});
